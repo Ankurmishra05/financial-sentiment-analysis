aadea6818078b0fdf799c0b6ba4e5cfdfb1a2a94
Ankur Mishra <ankurankit51@gmail.com>
2026-04-01 10:59:37 +0530

    added google drive model link

--- src/predict.py
+++ src/predict.py
@@ -7,7 +7,7 @@ MODEL_PATH = "models/finbert_model"
 if not os.path.exists(MODEL_PATH):
     os.makedirs("models", exist_ok=True)
 
-    url = "PASTE_YOUR_DRIVE_DIRECT_LINK_HERE"
+    url = "https://drive.google.com/file/d/13WAsFFupP_Ju5JcqsPTZnMm9CaQt5exn/view?usp=sharing"
     output = "models/model.zip"
 
     gdown.download(url, output, quiet=False)
