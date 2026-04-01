e93f315a7a1d8357fc324df2e36e367525b348b5
Ankur Mishra <ankurankit51@gmail.com>
2026-04-01 12:05:52 +0530

    removed local zip file

--- src/predict.py
+++ src/predict.py
@@ -1,9 +1,30 @@
+import os
+import gdown
+import zipfile
 from transformers import BertTokenizer, BertForSequenceClassification
 import torch
-import os
 
 MODEL_PATH = "models/finbert_model"
 
+# Download & extract if not exists
+if not os.path.exists(MODEL_PATH):
+    os.makedirs("models", exist_ok=True)
+
+    url = "https://drive.google.com/uc?id=13WAsFFupP_Ju5JcqsPTZnMm9CaQt5exn"
+    output = "models/model.zip"
+
+    gdown.download(url, output, quiet=False)
+
+    with zipfile.ZipFile(output, 'r') as zip_ref:
+        zip_ref.extractall("models")
+
+    os.remove(output)
+
+# 🔥 FIX: ensure correct path
+if not os.path.exists(MODEL_PATH):
+    raise Exception("Model folder not found after extraction")
+
+# Load model
 model = BertForSequenceClassification.from_pretrained(MODEL_PATH)
 tokenizer = BertTokenizer.from_pretrained(MODEL_PATH)
 
