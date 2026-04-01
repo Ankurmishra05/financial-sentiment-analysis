2591ddb9a1cbacc894b9fdb4e85322bbc7a59c4d
Ankur Mishra <ankurankit51@gmail.com>
2026-04-01 11:15:34 +0530

    fixed drive link

--- src/predict.py
+++ src/predict.py
@@ -7,10 +7,13 @@ MODEL_PATH = "models/finbert_model"
 if not os.path.exists(MODEL_PATH):
     os.makedirs("models", exist_ok=True)
 
-    url = "https://drive.google.com/file/d/13WAsFFupP_Ju5JcqsPTZnMm9CaQt5exn/view?usp=sharing"
+    url = "https://drive.google.com/uc?id=13WAsFFupP_Ju5JcqsPTZnMm9CaQt5exn"
     output = "models/model.zip"
 
     gdown.download(url, output, quiet=False)
+
+    if not zipfile.is_zipfile(output):
+        raise Exception("Download failed. Not a zip file.")
 
     with zipfile.ZipFile(output, 'r') as zip_ref:
         zip_ref.extractall("models")
